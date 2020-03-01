1b405a61a3ec1a5a6d95211f82fe41966ef487b3
Ahad Birang <farnabaz@gmail.com>
2020-03-01 12:12:39 +0330

    chore: generate docs

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,3 +1,12 @@
-import { configure } from '@storybook/svelte'
+import { configure, addParameters } from '@storybook/svelte'
+import pkg from '../package.json';
+
+// Option defaults:
+addParameters({
+  options: {
+    name: `${pkg.name} ${pkg.version}`,
+    url: 'https://github.com/jabama-co/svelte-admin',
+  },
+});
 
 configure(require.context('../.stories', true, /\.stories\.[jt]s$/), module)
